5c5f3374b9d840d140acfb630358ee2cf41b7d78
Kyohei <kyohei1212000@gmail.com>
2021-01-17 16:25:37 -0800

    Hackathon Submission version.
    Hackathon Submission version.

--- BackgroundBoard.js
+++ BackgroundBoard.js
@@ -7,15 +7,19 @@ class BackgroundBoard extends Phaser.Scene {
     }
     
     preload(){
+        this.load.image('background', 'assets/background.png')
         this.load.image('backgroundGrid', 'assets/gridBackground.png');
-        this.load.image('square1', 'assets/square1.png')
-        this.load.image('square2', 'assets/square2.jpg')
+        this.load.image('square1', 'assets/rock2.png')
+        this.load.image('square2', 'assets/bomb2.png')
+        this.load.image('blackSquare', 'assets/whiteSquare.jpg')
         this.firsttime = true;
     }
     
     create(){
         
+        this.add.image(800/2, 450/2, 'background')
         this.reachedBottom = false;
+        this.bombDropped = 2;
         this.add.text(20, 20, "Playing game");
         var bg = this.add.sprite(0,0,'backgroundGrid');
         bg.setOrigin(0,0)
@@ -25,7 +29,7 @@ class BackgroundBoard extends Phaser.Scene {
 
         
         var square1 = this.add.image(-w/2, -h/2, 'square1') //x=0, y=2
-        var square2 = this.add.image(w/2, -h/2, 'square2')  //x=1, y=2
+        var square2 = this.add.image(w/2, -h/2, 'square1')  //x=1, y=2
         square1.displayWidth = w;
         square1.displayHeight = h;
         square2.displayWidth = w;
@@ -54,9 +58,9 @@ class BackgroundBoard extends Phaser.Scene {
         var keyObj2 = this.input.keyboard.addKey('DOWN');  // Get key object
         keyObj2.on('down', this.handleKeyDown, this);
         var keyObj3 = this.input.keyboard.addKey('RIGHT');  // Get key object
-        keyObj3.on('down', function(event) { console.log('right is pressed') });
+        keyObj3.on('down', this.handleKeyRight, this);
         var keyObj4 = this.input.keyboard.addKey('LEFT');  // Get key object
-        keyObj4.on('down', function(event) { console.log('left is pressed') });
+        keyObj4.on('down', this.handleKeyLeft, this);
 
  
     }
@@ -64,20 +68,58 @@ class BackgroundBoard extends Phaser.Scene {
     update(){
     }
     
+    handleKeyRight(){
+        console.log('right is pressed');
+        this.gb1 = this.gb1.movePieceToRight();
+        this.gp1.leftx -= 1
+        this.gp1.rightx -= 1
+        this.gb1.printBoard();
+        this.renderAll(this.gb1);
+    }
+    
+    handleKeyLeft(){
+        console.log('left is pressed');
+        this.gb1 = this.gb1.movePieceToLeft();
+        this.gp1.leftx += 1
+        this.gp1.rightx += 1
+        this.gb1.printBoard();
+        this.renderAll(this.gb1);
+    }
+    
     handleKeyDown(){
             console.log('down is pressed');
 //            this.gp1.moveDown(this.gb1);
             var w = 224/10
             var h = 224/10
-            
+
 
             this.gp1.ycoord += 1
             var updateResult = this.gb1.updateBoard(this.gp1.leftx, this.gp1.ycoord)
             this.gp1.container.y += h
             if (updateResult == 'bottomReached'){
                 this.reachedBottom = true;
+                if(this.bombDropped == 0){
+                    console.log('bomb exploding')
+                    this.gb1 = this.gb1.bombExplode();
+                    this.renderAll(this.gb1)
+                    this.bombDropped = 2;
+                    
+                    this.gb1.printBoard()
+                }else if(this.bombDropped == 1){
+                    console.log('bomb explode next')
+                    this.bombDropped -= 1;
+                    
+                    this.gb1.printBoard()
+                    this.renderAll(this.gb1)
+                }else{
+                    this.gb1.printBoard()
+                    this.renderAll(this.gb1)
+                }
+            }else{
+                    this.gb1.printBoard()
+                    this.renderAll(this.gb1)
             }
-            this.gb1.printBoard()
+
         
 //            console.log(this.gp1.leftx + ' ' + this.gp1.rightx)
 
@@ -98,29 +140,92 @@ class BackgroundBoard extends Phaser.Scene {
                     var leftx = this.gp1.leftx
                     var rightx = this.gp1.rightx
                 
-                
-                    var square1 = this.add.image(-w/2 + (this.gp1.leftx)*w, -h/2, 'square1') 
-                    var square2 = this.add.image(w/2 + (this.gp1.rightx-1)*w, -h/2, 'square2')  
-                    square1.displayWidth = w;
-                    square1.displayHeight = h;
-                    square2.displayWidth = w;
-                    square2.displayHeight = h;
-                    var container = this.add.container(w, h, [square1, square2]);
-                    container.setSize(2*w, h)
-                    this.gp1 = new gamePiece(container)
-                    this.gp1.leftx = leftx
-                    this.gp1.rightx = rightx
-                    this.gp1.ycoord = 2
-                
-                
-                    this.gb1.spawnNewGamePiece(leftx, rightx);
-                    this.gb1.printBoard()
+                    var spawnBomb = (Math.random() > 0.5)? true:false;
+                    
+                    //spawning regular piece
+                    if(!spawnBomb){
+                                            
+                        var square1 = this.add.image(-w/2 + (this.gp1.leftx)*w, -h/2, 'square1') 
+                        var square2 = this.add.image(w/2 + (this.gp1.rightx-1)*w, -h/2, 'square1')  
+                        square1.displayWidth = w;
+                        square1.displayHeight = h;
+                        square2.displayWidth = w;
+                        square2.displayHeight = h;
+                        var container = this.add.container(w, h, [square1, square2]);
+                        container.setSize(2*w, h)
+                        this.gp1 = new gamePiece(container)
+                        this.gp1.leftx = leftx
+                        this.gp1.rightx = rightx
+                        this.gp1.ycoord = 2
+
+
+                        this.gb1.spawnNewGamePiece(leftx, rightx);
+                        this.gb1.printBoard()                        
+                    }else{
+                        if (this.bombDropped == 2){
+                            console.log('bomb explode next next')
+                            this.bombDropped = 1;
+                        }
+                        
+                        var square1 = this.add.image(-w/2 + (this.gp1.leftx)*w, -h/2, 'square1') 
+                        var square2 = this.add.image(w/2 + (this.gp1.rightx-1)*w, -h/2, 'square2')  
+                        square1.displayWidth = w;
+                        square1.displayHeight = h;
+                        square2.displayWidth = w;
+                        square2.displayHeight = h;
+                        var container = this.add.container(w, h, [square1, square2]);
+                        container.setSize(2*w, h)
+                        this.gp1 = new gamePiece(container)
+                        this.gp1.leftx = leftx
+                        this.gp1.rightx = rightx
+                        this.gp1.ycoord = 2
+
+
+                        this.gb1.spawnPieceWithBomb(leftx, rightx);
+                        this.gb1.printBoard()
+                    }
+
 
             }
             console.log("++++++++++++++++++++++++++++++++++++++++")
             this.reachedBottom = false;
         
             
+    }
+    
+    renderAll(gb){
+        var w = 224/10
+        var h = 224/10
+        
+//        var square3 = this.add.image(224/2 , 224/2 , 'blackSquare')
+//        square3.displayWidth = 224;
+//        square3.displayHeight = 224;
+        
+        for (var i=0; i < gb.boardArray.length; i++){
+            for (var j=0; j < gb.boardArray[j].length; j++){
+                if (gb.boardArray[i][j] == 's'){
+                    var square1 = this.add.image(-w/2 + (j+1)*w, -h/2 + (i-2+1)*h, 'square1') 
+                    square1.displayWidth = w;
+                    square1.displayHeight = h;
+                }else if (gb.boardArray[i][j] == 'e'){
+                    var square2 = this.add.image(-w/2 + (j+1)*w, -h/2 + (i-2+1)*h, 'square2') 
+                    square2.displayWidth = w;
+                    square2.displayHeight = h;
+                }else if (gb.boardArray[i][j] == 'x1' || gb.boardArray[i][j] == 'x2'){
+                    var square1 = this.add.image(-w/2 + (j+1)*w, -h/2 + (i-2+1)*h, 'square1') 
+                    square1.displayWidth = w;
+                    square1.displayHeight = h;
+                }else if (gb.boardArray[i][j] == 'b2'){
+                    var square2 = this.add.image(-w/2 + (j+1)*w, -h/2 + (i-2+1)*h, 'square2') 
+                    square2.displayWidth = w;
+                    square2.displayHeight = h;
+                }else{
+                    var square3 = this.add.image(-w/2 + (j+1)*w, -h/2 + (i-2+1)*h, 'blackSquare')
+                    square3.displayWidth = h;
+                    square3.displayHeight = h;
+                }
+            }
+        }
     }
     
 }
--- modules/gameBoard.js
+++ modules/gameBoard.js
@@ -35,10 +35,10 @@ class gameBoard {
             console.log('hasCollision at very bottom')
             return true
         }
-        if (typeof(this.boardArray[y+1][x]) == 'string' && this.boardArray[y+1][x].includes('s') ){
+        if ((typeof(this.boardArray[y+1][x]) == 'string' && this.boardArray[y+1][x].includes('s') ) || (typeof(this.boardArray[y+1][x]) == 'string' && this.boardArray[y+1][x].includes('e') )){
             console.log('1=====' + x + ' , ' + y)
             return true
-        }else if (typeof(this.boardArray[y+1][x+1]) == 'string' && this.boardArray[y+1][x+1].includes('s')){
+        }else if ((typeof(this.boardArray[y+1][x+1]) == 'string' && this.boardArray[y+1][x+1].includes('s')) || typeof(this.boardArray[y+1][x+1]) == 'string' && this.boardArray[y+1][x+1].includes('e')){
             console.log('2=====')
             return true
         }else{
@@ -59,7 +59,7 @@ class gameBoard {
                     var ele = this.boardArray[i][j];
                     
                     if (typeof(ele) == 'string'){
-                        if (typeof(ele) == 'string' && ele == 's'){
+                        if ((typeof(ele) == 'string' && ele == 's') || (typeof(ele) == 'string' && ele == 'e')){
                             console.log('this is solid block and not moving down anymore.')
                         }else{
                             console.log('this is not solid so block goes down')
@@ -76,10 +76,20 @@ class gameBoard {
             for (var i = this.boardArray.length - 2; i >= 0 ; i--){
                 for (var j = 0; j < this.boardArray[i].length ;j++){
                     var ele = this.boardArray[i][j];
-                    if (typeof(ele) == 'string' && ele != 's'){
-                        this.boardArray[i+1][j] = ele;
-                        this.boardArray[i][j] = undefined;
+                    
+                    if (typeof(ele) == 'string'){
+                        if ((typeof(ele) == 'string' && ele == 's') || (typeof(ele) == 'string' && ele == 'e')){
+                            var eleBelow = this.boardArray[i+1][j];
+                            if ((typeof(eleBelow) == 'string' && eleBelow == 's') || (typeof(eleBelow) == 'string' && eleBelow == 'e')){
+                                console.log('this is solid block and not moving down anymore.')   
+                            }
+                        }else{
+                            console.log('this is not solid so block goes down.')
+                            this.boardArray[i+1][j] = ele;
+                            this.boardArray[i][j] = undefined;    
+                        }
                     }
+                     
                 }
             }
             this.printBoard()
@@ -89,6 +99,8 @@ class gameBoard {
                     var ele = this.boardArray[i][j];
                     if (typeof(ele) == 'string' && ele.includes('x')){
                         this.boardArray[i][j] = 's';   
+                    }else if (typeof(ele) == 'string' && ele.includes('b')){
+                        this.boardArray[i][j] = 'e';
                     }
                 }
             }
@@ -96,6 +108,49 @@ class gameBoard {
         
         return returnStatement
     }            
+    
+    movePieceToRight(){
+            for (var i = this.boardArray.length - 2; i >= 0 ; i--){
+                for (var j = this.boardArray[i].length - 1; j >= 0  ;j--){
+                    var ele = this.boardArray[i][j];
+                    
+                    if (typeof(ele) == 'string'){
+                        if ((typeof(ele) == 'string' && ele == 's') || (typeof(ele) == 'string' && ele == 'e')){
+                            console.log('this block is solid')
+                        }else{
+                            if (j+1 < 10){
+                                this.boardArray[i][j+1] = ele;
+                                this.boardArray[i][j] = undefined;    
+                            }
+                        }
+                    }
+                     
+                }
+            }
+        
+        return this
+    }
+    
+    movePieceToLeft(){
+        for (var i = this.boardArray.length - 2; i >= 0 ; i--){
+            for (var j = 0; j < this.boardArray[i].length-1  ;j++){
+                var ele = this.boardArray[i][j];
+
+                if (typeof(ele) == 'string'){
+                    if ((typeof(ele) == 'string' && ele == 's') || (typeof(ele) == 'string' && ele == 'e')){
+                        console.log('this block is solid')
+                    }else{
+                        if (j-1 >= 0){
+                            this.boardArray[i][j-1] = ele;
+                            this.boardArray[i][j] = undefined;    
+                        }
+                    }
+                }
+
+            }
+        }
+        return this
+    }
 
     spawnNewGamePiece(x1, x2){
             console.log(x1 + ' : ' + x2 + ' spawning')
@@ -103,6 +158,38 @@ class gameBoard {
             this.boardArray[2][x2] = 'x2'
     }
 
+    spawnPieceWithBomb(x1, x2){
+            console.log(x1 + ' : ' + x2 + ' spawning with bomb')
+            this.boardArray[2][x1] = 'x1'
+            this.boardArray[2][x2] = 'b2'   
+    }
+    
+    bombExplode(){
+        for (var i = this.boardArray.length - 1; i >= 0 ; i--){
+            for (var j = 0; j < this.boardArray[i].length ;j++){
+                var ele = this.boardArray[i][j];
+                if (typeof(ele) == 'string' && ele.includes('e')){
+                    this.boardArray[i][j] = undefined;
+                    if (i-1 > 0){
+                        this.boardArray[i-1][j-1] = undefined;
+                        this.boardArray[i-1][j] = undefined;
+                        this.boardArray[i-1][j+1] = undefined;
+                    }
+                    this.boardArray[i][j-1] = undefined;
+                    this.boardArray[i][j+1] = undefined;
+                    if (i < 12 - 1){
+                        this.boardArray[i+1][j-1] = undefined;
+                        this.boardArray[i+1][j] = undefined;
+                        this.boardArray[i+1][j+1] = undefined;               
+                    }
+
+                }
+            }
+        }
+        
+        return this
+    }
+    
     
 
 
